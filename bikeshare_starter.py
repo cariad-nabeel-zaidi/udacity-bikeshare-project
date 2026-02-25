c6ebbddf8a8fa9bc29481f258c86a371b592e346
Syed Muhammad Nabeel Zaidi <syed.muhammad.nabeel.zaidi@cariad.technology>
2026-02-25 09:00:24 +0100

    adjusted the indentation of comment to get user input citymonthweek

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
@@ -16,14 +17,12 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+   
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
 
     # get user input for month (all, january, february, ... , june)
 
-
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
 
     print('-'*40)
     return city, month, day
